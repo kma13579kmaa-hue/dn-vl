aa69e57f78eafd8ee7324f5fb26c87c44a906e76
kma13579kmaa-hue <kma13579.kmaa@gmail.com>
2026-01-01 22:49:51 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '55e55450-3050-4501-be49-ec7d9a772952'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
